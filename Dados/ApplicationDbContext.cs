61d0a9087fe1ec4c8e308449cc4c52664c201c26
juliano <julianoamaralferreira@gmail.com>
2019-09-11 11:26:12 -0300

    Funcionando migation database

---
+++ Dados/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+using Dominio.entidades;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dados
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<produto> Produtos{get; set;}
+    }
+}
